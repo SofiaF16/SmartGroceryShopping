f83a2a4b503d77116370846ac90c44e5189d5032
SofiaF16 <sofiaf95@gmail.com>
2021-10-29 14:34:29 -0700

    Added CartActivity and OrderHistoryActivity

---
+++ app/src/main/java/com/example/f21g3_smartgroceryshopping/OrdersHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.f21g3_smartgroceryshopping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrdersHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orders_history);
+    }
+}

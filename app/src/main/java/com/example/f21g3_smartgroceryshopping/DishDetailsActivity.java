29cd5e3a5a6ad696309610f42186e40dedf02b2a
SofiaF16 <sofiaf95@gmail.com>
2021-11-04 23:34:07 -0700

    Added activity_dish_details and activity_orders_history layouts and implemented logic of a processing dialog

--- app/src/main/java/com/example/f21g3_smartgroceryshopping/DishDetailsActivity.java
+++ app/src/main/java/com/example/f21g3_smartgroceryshopping/DishDetailsActivity.java
@@ -1,13 +1,21 @@
 package com.example.f21g3_smartgroceryshopping;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.View;
+
 
 import com.example.f21g3_smartgroceryshopping.viewmodel.DishDetailsViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -17,6 +25,9 @@ public class DishDetailsActivity extends AppCompatActivity {
     private static final String DISH_ID = "DishDetailsActivityDishId";
 
     private int dishId;
+    FloatingActionButton fabOrder;
+    ProgressDialog progressDialog;
+    Toolbar toolbar;
 
     private DishDetailsViewModel dishDetailsViewModel;
 
@@ -24,18 +35,44 @@ public class DishDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dish_details);
-
         dishId = getIntent().getIntExtra(DISH_ID, dishId);
+
+        fabOrder = findViewById(R.id.fabOrder);
+        toolbar = findViewById(R.id.toolbarOrder);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline);
+        setSupportActionBar(toolbar);
+
+        fabOrder.setOnClickListener((View view) -> {
+
+            progressDialog = new ProgressDialog(DishDetailsActivity.this);
+            progressDialog.show();
+            progressDialog.setContentView(R.layout.loading_page);
+            progressDialog.setCancelable(false);
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    progressDialog.dismiss();
+                    setContentView(R.layout.order_successful);
+                }
+            }, 5000);
+
+        });
+
 
         dishDetailsViewModel = new ViewModelProvider(this).get(DishDetailsViewModel.class);
     }
-
 
     public static void launch(Context context, int dishId) {
         Intent intent = new Intent(context, DishDetailsActivity.class);
         intent.putExtra(DISH_ID, dishId);
         context.startActivity(intent);
     }
+
+
+
+
 
 
 }
--- app/src/main/java/com/example/f21g3_smartgroceryshopping/OrdersHistoryActivity.java
+++ app/src/main/java/com/example/f21g3_smartgroceryshopping/OrdersHistoryActivity.java
@@ -1,14 +1,32 @@
 package com.example.f21g3_smartgroceryshopping;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
 
 public class OrdersHistoryActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders_history);
+
+        toolbar = findViewById(R.id.toolbarHistory);
+        toolbar.setTitle(R.string.txtTitleHistory);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline);
+
+
     }
+
+
+
+
 }
